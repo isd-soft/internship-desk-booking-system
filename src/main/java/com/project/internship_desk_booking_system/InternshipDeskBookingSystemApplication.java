a819c06b1dabe53b41df1e59707a23e3c74fc7aa
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-03 12:03:27 +0200

    Init commit

---
+++ src/main/java/com/project/internship_desk_booking_system/InternshipDeskBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.internship_desk_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipDeskBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipDeskBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/internship_desk_booking_system/InternshipDeskBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.internship_desk_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipDeskBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
