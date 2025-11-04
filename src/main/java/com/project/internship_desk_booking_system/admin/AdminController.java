1b3fcc303a4c2700cc03d51d89b53ce467952e4b
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-04 14:11:21 +0200

    created edit desk method for controller

--- src/main/java/com/project/internship_desk_booking_system/admin/AdminController.java
+++ src/main/java/com/project/internship_desk_booking_system/admin/AdminController.java
@@ -3,6 +3,7 @@ package com.project.internship_desk_booking_system.admin;
 import com.project.internship_desk_booking_system.DTO.DeskDTO;
 import com.project.internship_desk_booking_system.entity.Desk;
 import jakarta.validation.Valid;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -34,5 +35,11 @@ public class AdminController {
         return ResponseEntity
                 .ok(adminService.deactivateDesk(deskId));
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/edit")
+    public ResponseEntity<Desk> editDesk(
+            @RequestBody Desk desk
+    ) throws ChangeSetPersister.NotFoundException {
+        return ResponseEntity.ok(adminService.editDesk(desk));
+    }
 }
