4a4483d2e5b33c02c0767c54b3893f5a37f1643e
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-04 11:38:01 +0200

    created admin controller with 2 endpoints

---
+++ src/main/java/com/project/internship_desk_booking_system/admin/AdminController.java
@@ -0,0 +1,38 @@
+package com.project.internship_desk_booking_system.admin;
+
+import com.project.internship_desk_booking_system.DTO.DeskDTO;
+import com.project.internship_desk_booking_system.entity.Desk;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(
+          AdminService adminService
+    ){
+        this.adminService = adminService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/addDesk")
+    public ResponseEntity<Desk> addDesk(
+            @RequestBody @Valid DeskDTO deskDTO
+    ){
+        return ResponseEntity
+                .ok(adminService.addDesk(deskDTO));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/deactivateDesk/{id}")
+    public ResponseEntity<Desk> deactivateDesk(
+            @PathVariable Long deskId
+    ){
+        return ResponseEntity
+                .ok(adminService.deactivateDesk(deskId));
+    }
+}
