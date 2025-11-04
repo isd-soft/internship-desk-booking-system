a9637c0c64a85f9c048620fd0d0bb0f533db9dff
ifantaziu <roscairina012@gmail.com>
2025-11-04 10:20:43 +0200

    created table favourit desks

---
+++ src/main/resources/db/migration/V3__favourit-desks.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE id_favourite_desk_seq START 1;
+
+CREATE TABLE favourite_desks (
+    id INTEGER DEFAULT nextval('id_favourite_desk_seq') PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    desk_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_favourite_user
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_favourite_desk
+        FOREIGN KEY (desk_id)
+        REFERENCES desk (id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT unique_user_desk UNIQUE (user_id, desk_id)
+);
+
+CREATE INDEX idx_favourite_desks_user_id ON favourite_desks(user_id);
