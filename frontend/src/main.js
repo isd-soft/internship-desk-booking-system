873e8427498ad592542113cf183fc198c1b7cad9
Lilian <ghertaliliandeveloper@gmail.com>
2025-11-05 14:17:27 +0200

    add: (frontend) login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import VueKonva from 'vue-konva'
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).use(vuetify).use(VueKonva).mount("#app");
