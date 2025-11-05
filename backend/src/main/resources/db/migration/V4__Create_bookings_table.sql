c8f7be43ddf7d09ef5d99f7cdf9587960d66fce1
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-05 12:40:45 +0200

    fixed table name and made FK to users

--- backend/src/main/resources/db/migration/V4__Create_bookings_table.sql
+++ backend/src/main/resources/db/migration/V4__Create_bookings_table.sql
@@ -1,10 +1,15 @@
 CREATE SEQUENCE booking_id_seq START 1;
 
-CREATE TABLE Booking(
-    id integer default nextval('booking_id_seq') primary key,
+CREATE TABLE booking (
+    id INTEGER DEFAULT nextval('booking_id_seq') PRIMARY KEY,
     user_id INT NOT NULL,
     desk_id INT NOT NULL,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
-    status VARCHAR(20) DEFAULT 'CONFIRMED'
-)
+    status VARCHAR(20) DEFAULT 'CONFIRMED',
+
+    CONSTRAINT fk_booking_user
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
+);
