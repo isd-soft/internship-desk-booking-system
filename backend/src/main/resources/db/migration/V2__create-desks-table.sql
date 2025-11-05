a80ae1991c8a17eef7de1db1b64e9aca099ff506
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-05 12:41:04 +0200

    small fix for column names to match entity

--- backend/src/main/resources/db/migration/V2__create-desks-table.sql
+++ backend/src/main/resources/db/migration/V2__create-desks-table.sql
@@ -3,13 +3,13 @@ CREATE TABLE desk (
                       id integer default nextval('id_desk_seq') PRIMARY KEY ,
                       desk_name VARCHAR(50) NOT NULL UNIQUE,
                       zone VARCHAR(100) NOT NULL,
-                      type VARCHAR(20) NOT NULL DEFAULT 'SHARED',
-                      status VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
+                      desk_type VARCHAR(20) NOT NULL DEFAULT 'SHARED',
+                      desk_status VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
                       is_temporarily_available BOOLEAN NOT NULL DEFAULT false,
                       temporary_available_from TIMESTAMP,
                       temporary_available_until TIMESTAMP,
 
-                      CONSTRAINT chk_desk_type CHECK (type IN ('ASSIGNED', 'SHARED', 'UNAVAILABLE')),
-                      CONSTRAINT chk_desk_status CHECK (status IN ('ACTIVE', 'DEACTIVATED'))
+                      CONSTRAINT chk_desk_type CHECK (desk_type IN ('ASSIGNED', 'SHARED', 'UNAVAILABLE')),
+                      CONSTRAINT chk_desk_status CHECK (desk_status IN ('ACTIVE', 'DEACTIVATED'))
 );
 
