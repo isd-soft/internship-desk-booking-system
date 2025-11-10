d3f127800bb61f9f7fadfb615b54c038aaab7d05
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-10 20:03:04 +0200

    changed migrations

--- backend/src/main/resources/db/migration/V2__create_zone_table.sql
+++ backend/src/main/resources/db/migration/V2__create_zone_table.sql
@@ -1,9 +1,18 @@
-CREATE SEQUENCE zone_id_seq START 1;
 CREATE TABLE zone (
-                      id INTEGER DEFAULT nextval('zone_id_seq') PRIMARY KEY,
-                      zone_name VARCHAR(100) NOT NULL UNIQUE,
-                      zone_abbreviation VARCHAR(10) NOT NULL UNIQUE,
-                      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-                      CONSTRAINT chk_zone_abbr_length CHECK (LENGTH(zone_abbreviation) <= 10)
+    id SERIAL PRIMARY KEY,
+    zone_name VARCHAR(100) NOT NULL UNIQUE,
+    zone_abbreviation VARCHAR(10) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT chk_zone_abbr_length CHECK (LENGTH(zone_abbreviation) <= 10)
 );
+
+INSERT INTO zone(zone_name, zone_abbreviation)
+VALUES
+    ('Development', 'DEV'),
+    ('Engineering', 'ENG'),
+    ('Administration', 'ADM'),
+    ('Marketing', 'MARK'),
+    ('Power Line Communication', 'PLC'),
+    ('Service', 'SER'),
+    ('Management', 'MAN'),
+    ('Front end', 'FRONT');
