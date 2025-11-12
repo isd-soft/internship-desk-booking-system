aa25cf103330f238ffeb41867d706b5b86fad27e
Vladimir.Ryjenco <vladimir.ryjenco@gmail.com>
2025-11-12 14:30:29 +0200

    Test migrations just for comfort

---
+++ backend/src/main/resources/db/migration/V9__Insert_users_test_data.sql
@@ -0,0 +1,72 @@
+INSERT INTO users (first_name, last_name, email, role, password_hash)
+VALUES
+(
+    'Admin',
+    'User',
+    'admin@example.com',
+    'ADMIN',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'John',
+    'Doe',
+    'test@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Alexander',
+    'Ivanov',
+    'test2@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Maria',
+    'Popescu',
+    'test3@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Andrei',
+    'Petrov',
+    'test4@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Elena',
+    'Ionescu',
+    'test5@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Dmitri',
+    'Sokolov',
+    'test6@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Ana',
+    'Rusu',
+    'test7@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Vladimir',
+    'Koval',
+    'test8@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+),
+(
+    'Ioana',
+    'Marin',
+    'test9@example.com',
+    'USER',
+    '$2a$12$vqy.rV4j2DlPt97g/uqjweo9ZoWY7QP7Od2jK.l/cYMXGyD8TZP9e' --'testpassword'
+);
