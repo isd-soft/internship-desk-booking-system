c5e0c1fe29d69805799d45a335325dbab79ed260
Lilian <ghertaliliandeveloper@gmail.com>
2025-11-24 14:49:45 +0200

    ready to deploy

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -3,7 +3,7 @@ import { clearAuthData } from "../utils/auth";
 import router from "../router";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://localhost:8000/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
